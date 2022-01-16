647d442195026924213f0f2043484cd28cd5bfd8
Jacob Namhyung Kim <everywherejacobkim@gmail.com>
2022-01-15 23:49:10 -0800

    feat: create header component & mapping data

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Header = ({keywords}) => {
+    return(
+        <div className="header-container">
+            {
+                keywords.map((key,id)=>{
+                    {
+                        return <button key={id}>{key}</button>
+                    }
+                    
+                })
+            }
+        </div>
+    );
+}
+
+export default Header;
